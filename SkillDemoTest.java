84b39e8e8faa9cffd7cfddda9a9de2b61fbaa6cd
helenahundhausen <helenahundhausen@gmail.com>
2022-02-01 20:34:04 -0800

    Test Fixed!

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1, 3));
+        assertEquals(2, SkillDemo.add(1, 1));
     }
 }
